2610094a22bcf91af6b899fd8a1cfbea63f13e0c
Mr-Hunter <1522138135@qq.com>
2019-07-01 03:55:05 -0700

    测试

---
+++ test.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name:    test.cpp
+	> Author:       Hunter
+	> Mail:         hunter.520@qq.com
+	> Created Time: 2019年07月01日 星期一 03时53分56秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello World !!! \n"<<endl;
+    return 0;
+}
+
